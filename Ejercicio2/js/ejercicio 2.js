a2e5ab3c4d3db66966b0a06a5144517631a7275a
ElizaPl21 <elipila21@gmail.com>
2023-09-26 07:39:39 -0600

    Creando repositorio

---
+++ Ejercicio2/js/ejercicio 2.js
@@ -0,0 +1,27 @@
+const createCard=() =>{
+    const card = document.createElement('div');
+    card.classList.add('profile', 'container');
+    return card;
+
+}
+
+const inputName = document.getElementById('name');
+const userName = document.getElementById('username');
+
+const inputEdad = document.getElementById('edad');
+const age = document.getElementById('age');
+
+const inputDescribir = document.getElementById('describir');
+const describir = document.getElementById('descripcion');
+
+
+const profileBtn = document.getElementById('ProfileBtn');
+
+profileBtn.addEventListener('click', () => {
+    userName.textContent = inputName.value;
+    age.textContent = inputEdad.value;
+    describir.textContent = inputDescribir.value;
+
+
+})
+
